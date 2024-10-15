3f25f7303f67770c5c5008a14f9ff3cb58bc612c
Five <kylemuse08@gmail.com>
2024-10-15 22:16:22 +0300

    Completed the xoxo challenge

---
+++ JS/xoxo.js
@@ -0,0 +1,29 @@
+// this is a program that returns true if a str has same number of x's and o's
+//It is not case sensitive
+// "xoxo" returns true
+//"xxxooo" returns true
+//"xxoooo" returns false
+// str can have other letters "xopshnxo"
+
+function XO(str) {
+  str = str.toLowerCase();
+  var strx = [];
+  var stro = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "x") {
+      strx.unshift(str[i]);
+    } else if (str[i] === "o") {
+      stro.unshift(str[i]);
+    } else {
+      continue;
+    }
+  }
+  if (stro.length === strx.length) {
+    console.log(true);
+    return true;
+  } else {
+    console.log(false);
+    return false;
+  }
+}
+XO("XOxpxo");
